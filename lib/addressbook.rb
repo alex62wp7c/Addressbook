63e885d7e63531f08b816bd6bc3a3ae8f2348996
Alex Huang <alex62wp7c@gmail.com>
2013-05-28 16:17:38 -0400

    <adding photos commit>

--- lib/addressbook.rb
+++ lib/addressbook.rb
@@ -1,20 +1,21 @@
 class Entry
-	attr_accessor :name, :phoneNumber, :address
+	attr_accessor :name, :phoneNumber, :address, :profilePic
 
-	def initialize (name, phoneNumber, address, birthday, notes)
+	def initialize (name, phoneNumber, address, birthday, time, picture)
 		@name = name
 		@phoneNumber = phoneNumber
 		@address = address
 		@birthday = birthday
-		@notes = notes
+		@schedule = time
+		@profilePic = picture
 	end
 
 	def getBirthday
 		return "My name is " + @name + " and my birthday is on " + @birthday
 	end
 
-	def getNotes
-		return @notes
+	def getSchedule
+		return @schedule
 	end
 end
 
